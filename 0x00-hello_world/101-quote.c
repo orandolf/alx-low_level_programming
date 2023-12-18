03aae2fa6004a911a91dbd399ff497f1f00bf381
Randolf Boampong <orandolf2@gmail.com>
2023-12-18 00:51:46 -0800

    101-quote.c#

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <unistd.h>
 /**
  * main - A C program that prints a line to the standard error
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-        fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" , stdout);
+        write(2,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" , 59);
         return (1);
 }

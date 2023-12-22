9aed8288dde5ce1936664d0459b6ecc393a1bc9d
Randolf Boampong <orandolf2@gmail.com>
2023-12-22 08:42:03 -0800

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,17 @@
 #include "main.h"
 
 /**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        print_most_numbers();
+        return (0);
+}
+
+/**
  * print_most_numbers - prints 01356789
  *
  * Return: void
